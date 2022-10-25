489aa939c21793b502e4c8d1f85e59dcf5ec6f8f
sara.alloubani <saralubani@gmail.com>
2022-10-25 11:56:46 +0300

    assigment 16 javacript

---
+++ javascript/assigment 16.js
@@ -0,0 +1,7 @@
+onload=function()
+{
+document.querySelector("h1").style.background="pink";
+document.getElementsByClassName("p")[0].style.background="blue";
+document.getElementById("p").style.background="red";
+
+}
